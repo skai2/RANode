1732d18742189876cbbf46c9b7a6c12baef4f977
SKai2 <cpiantino@gmail.com>
2018-10-10 19:05:16 -0300

    Encapsulation and usability modifications

--- node.py
+++ node.py
@@ -7,144 +7,146 @@ import random
 import time
 
 
-
-# VARS -------------------------------------------------------------------------
+class Node():
+# MAIN -------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
+    def __init__(self):
+        self.MYNAME = str(random.randint(111111, 999999))
+        self.DISCOVERY_HOST = '<broadcast>'
+        self.DISCOVERY_PORT = 12345
+        self.NODE_HOST = Node.getIP()
+        self.NODE_PORT = random.randint(1025, 65535+1)
+        self.peerlist = {}
+        self.reversepeer = {}
 
-def getIP():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.connect(("8.8.8.8", 80))
-        return s.getsockname()[0]
+        self.discovering = Event()
+        self.listening = Event()
+        Thread(target=self.discoverer, args=(self.discovering,)).start()
+        Thread(target=self.listener, args=(self.listening,)).start()
+        print('---<<<[ (%s)-(%s, %d) ]>>>---' % \
+            (self.MYNAME, self.NODE_HOST, self.NODE_PORT))
 
-MYNAME = str(random.randint(111111, 999999))
-DISCOVERY_HOST = '<broadcast>'
-DISCOVERY_PORT = 12345
-NODE_HOST = getIP()
-NODE_PORT = random.randint(1025, 65535+1)
-peers = {}
+    def kill(self):
+        self.discovering.clear()
+        self.listening.clear()
 
-
+    def peers(self):
+        print("-----------------------------------------------")
+        for peer in self.peerlist.keys():
+            print("[(%s)-(%s, %d)]" % \
+                (peer, self.peerlist[peer][0], self.peerlist[peer][1]))
+        print("-----------------------------------------------")
 
 # LISTENERS ----------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 
-def discoverer(discovering):
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((DISCOVERY_HOST, DISCOVERY_PORT))
-        s.settimeout(1)
-        discovering.set()
-        while discovering.is_set():
-            try:
-                message, addr = s.recvfrom(1024)
-                Thread(target=handle_message, args=(message, addr,)).start()
-            except socket.timeout as e:
-                send_message('HeyBrah-'+NODE_HOST+'-'+str(NODE_PORT)+'-'+MYNAME, (DISCOVERY_HOST, DISCOVERY_PORT))
-                time.sleep(random.random())
-            except Exception as e:
-                print(e)
-        s.close()
+    def discoverer(self, discovering):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((self.DISCOVERY_HOST, self.DISCOVERY_PORT))
+            s.settimeout(1)
+            discovering.set()
+            while discovering.is_set():
+                try:
+                    message, addr = s.recvfrom(1024)
+                    Thread(target=self.handle_message, args=(message, addr,)).start()
+                except socket.timeout as e:
+                    self.send_message(0, 'HeyBrah-'+self.NODE_HOST+'-'+str(self.NODE_PORT)+'-'+self.MYNAME)
+                    time.sleep(random.random())
+                except Exception as e:
+                    print(e)
+            s.close()
 
-def listener(listening):
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((NODE_HOST, NODE_PORT))
-        s.settimeout(1)
-        listening.set()
-        while listening.is_set():
-            try:
-                message, addr = s.recvfrom(1024)
-                Thread(target=handle_message, args=(message, addr,)).start()
-            except socket.timeout as e:
-                pass
-            except Exception as e:
-                print(e)
-        s.close()
+    def listener(self, listening):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((self.NODE_HOST, self.NODE_PORT))
+            s.settimeout(1)
+            listening.set()
+            while listening.is_set():
+                try:
+                    message, addr = s.recvfrom(1024)
+                    Thread(target=self.handle_message, args=(message, addr,)).start()
+                except socket.timeout as e:
+                    pass
+                except Exception as e:
+                    print(e)
+            s.close()
 
-def handle_message(message, addr):
-    message = message.decode('utf-8')
-    split = message.split('-')
-    if split[0] == 'HeyBrah':
-        if not (split[1] == NODE_HOST and int(split[2]) == NODE_PORT):
-            peers[split[3]] = (split[1], int(split[2]))
-    else:
-        print('received', message, 'from', addr)
-
-
+    def handle_message(self, message, addr):
+        message = message.decode('utf-8')
+        split = message.split('-')
+        if split[0] == 'HeyBrah':
+            if not (split[1] == self.NODE_HOST and int(split[2]) == self.NODE_PORT):
+                self.peerlist[split[3]] = (split[1], int(split[2]))
+                self.reversepeer[(split[1], int(split[2]))] = split[3]
+        else:
+            print("---%s %s" % (self.reversepeer[addr], message))
 
 # SENDERS ----------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 
-def send_message(message, addr):
-    ip = addr[0]
-    port = addr[1]
-    try:
+    def getIP():
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.bind((NODE_HOST, NODE_PORT))
-            s.sendto(message.encode('utf-8'), (ip, port))
-    except Exception as e:
-        print(e)
+            s.connect(("8.8.8.8", 80))
+            return s.getsockname()[0]
+
+    def send_message(self, id, message):
+        if id == 0:
+            ip = self.DISCOVERY_HOST
+            port = self.DISCOVERY_PORT
+        else:
+            try:
+                ip = self.peerlist[str(id)][0]
+                port = self.peerlist[str(id)][1]
+            except KeyError as e:
+                print('--- <Invalid peer!>', e)
+                return
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                s.bind((self.NODE_HOST, self.NODE_PORT))
+                s.sendto(message.encode('utf-8'), (ip, port))
+        except Exception as e:
+            print(e)
 
 
 
-# MAIN -------------------------------------------------------------------------
+# CMD --------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 
-class Node(Cmd):
+class NodeCMD(Cmd):
     def do_quit(self, args):
         """Terminates the node."""
-        self.discovering.clear()
-        self.listening.clear()
+        self.node.kill()
         raise SystemExit
 
     def do_init(self, args=None):
         """Initializes the node."""
-        self.discovering = Event()
-        Thread(target=discoverer, args=(self.discovering,)).start()
-        self.listening = Event()
-        Thread(target=listener, args=(self.listening,)).start()
-        print('--<<<[ (%s)-(%s, %d) ]>>>--' % (MYNAME, NODE_HOST, NODE_PORT))
+        self.node = Node()
 
     def do_peers(self, args):
         '''Lists all peers.'''
-        for peer in peers.keys():
-            print("[(%s)-(%s, %d)]" % (peer, peers[peer][0], peers[peer][1]))
+        self.node.peers()
 
-    def do_message(self, args):
+    def do_to(self, args):
         """Sends a message."""
         args = args.split()
         message = 'test'
-        host = '<broadcast>'
-        port = DISCOVERY_PORT
-        if len(args) != 2:
-            print('invalid arguments!')
-            return
-        else:
-            try:
-                ip = peers[args[0]][0]
-                port = peers[args[0]][1]
-            except KeyError:
-                print('invalid peer!')
-                return
+        id = 0
+        if len(args) >= 1:
+            id = args[0]
+        if len(args) >= 2:
             message = args[1]
-        send_message(message, (ip, port))
-        # if len(args) > 0:
-        #     message = args[0]
-        # if len(args) > 1:
-        #     host = args[1]
-        # if len(args) > 2:
-        #     port = int(args[2])
-        # if len(args) > 3:
-        #     print('Too many arguments!')
-        #     return
-        # send_message(message, (host, port))
-
+        if len(args) >= 3:
+            for arg in args[2:]:
+                message = message + ' ' + arg
+        self.node.send_message(id, message)
 
 if __name__ == '__main__':
-    node = Node()
+    node = NodeCMD()
     node.do_init()
     node.prompt = ''
     node.cmdloop('')
